8a92e8a3c8f3172fcca3a5593b9ac3d386a81227
Rony Silva <rony.silva@grupoboticario.com.br>
2022-04-23 20:36:53 -0300

    chore: fixing lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,43 @@
 module.exports = {
-    extends: 'airbnb',
-    plugins: [
-      'react',
-      'react-native',
-      'react-hooks'
+  env: {
+    es6: true,
+  },
+  extends: [
+    'airbnb',
+    'prettier',
+    'prettier/react'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    __DEV__: 'readonly'
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'prettier'
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [
+      'warn',
+      {
+        extensions: ['.jsx', '.js']
+      }
     ],
-    parser: 'babel-eslint',
-    env: {
-      jest: true,
-      'react-native/react-native': true,
-    },
-    rules: {
-      'no-use-before-define': 'off',
-      'react/jsx-filename-extension': 'off',
-      'react/prop-types': 'off',
-      'comma-dangle': 'off',
-      'padded-blocks': 'off',
-      'arrow-body-style': 'off',
-      'react-hooks/exhaustive-deps': 'warn',
-      'react-hooks/exhaustive-deps': 'warn',
-      'react-native/no-unused-styles': 2,
-      'react-native/split-platform-components': 2,
-      'react-native/no-inline-styles': 2,
-      'react-native/no-color-literals': 2,
-      'react-native/no-raw-text': 2,
-      'react-native/no-single-element-style-arrays': 2,
-    },
-    globals: {
-      fetch: false
-    }
-  };
+    'import/prefer-default-export': 'off',
+    'react/state-in-constructor': 'off',
+    'react/static-property-placement': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react/prop-types': 'off',
+    'no-param-reassign': 'off',
+    'no-console': 'off'
+  },
+};
